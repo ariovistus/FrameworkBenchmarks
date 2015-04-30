f9a92ac01a275d933ba6c9699c817c84611c667f
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2015-04-30 07:16:04 -0700

    add vibe.d single database query, postgresql

--- frameworks/D/vibe.d/install.sh
+++ frameworks/D/vibe.d/install.sh
@@ -4,7 +4,7 @@
 fw_get 'http://downloads.dlang.org/releases/2.x/2.067.1/dmd_2.067.1-0_amd64.deb'
     -O 'dmd_2.067.1-0_amd64.deb'
 
-sudo apt-get install xdg-utils
+sudo apt-get -y install xdg-utils
 sudo dpkg -i ./dmd_2.067.1-0_amd64.deb
 
 fw_get 'https://github.com/rejectedsoftware/dub/archive/v0.9.23.tar.gz' \
--- frameworks/D/vibe.d/source/app.d
+++ frameworks/D/vibe.d/source/app.d
@@ -1,25 +1,32 @@
 import vibe.d;
+import std.process;
+import std.random;
+import ddb.postgres;
 
 
 shared static this()
 {
-	auto settings = new HTTPServerSettings;
-        auto router = new URLRouter;
-        router.registerRestInterface(new DaStuff);
-        router.get("/plaintext", &plaintext);
-	settings.port = 8081;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, router);
-
-	logInfo("Please open http://127.0.0.1:8081/ in your browser.");
+    auto settings = new HTTPServerSettings;
+    auto router = new URLRouter;
+    router.registerRestInterface(new DaStuff);
+    router.get("/plaintext", &plaintext);
+    settings.port = 8081;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    listenHTTP(settings, router);
 }
 
 class Message {
     string message;
 }
 
+class DBResult {
+    int id;
+    int randomNumber;
+}
+
 interface IDaStuff {
     @property Message json();
+    @property DBResult db();
 }
 
 class DaStuff : IDaStuff {
@@ -28,8 +35,33 @@ class DaStuff : IDaStuff {
         message.message = "Hello, World!";
         return message;
     }
+
+    @property DBResult db() {
+        auto result = new DBResult;
+        result.id = uniform!"[]"(1, 10000);
+
+        auto conn = new PGConnection([
+            "host": environment.get("DBHOST", ""),
+            "database": "hello_world",
+            "user": "benchmarkdbuser",
+            "password": "benchmarkdbpass",
+        ]);
+        scope(exit) conn.close();
+
+        auto cmd = new PGCommand(conn, 
+            "SELECT randomnumber FROM World where id=$1;");
+        cmd.parameters.add(1, PGType.INT4).value = result.id;
+        auto rows = cmd.executeQuery();
+        scope(exit) rows.close();
+
+        auto row = rows.front;
+        result.randomNumber = row["randomnumber"].get!int;
+
+        return result;
+    }
 }
 
 void plaintext(HTTPServerRequest request, HTTPServerResponse response) {
     response.writeBody("Hello, World!");
 }
+
